cdc054d19bef0cc5742cbbb4bd9055605b63e2a4
Riccardo Casatta <riccardo@casatta.it>
2017-08-22 09:55:33 +0200

    adding Phone number to address api spec

--- sync.sh
+++ sync.sh
@@ -1,2 +1,3 @@
 jekyll build
+#spectacle -t api/pn2a/ -l img/logos/eternity_logo_final.svg api/pn2a/spec.yaml
 rsync -vr --exclude node_modules/ _site/ riccardo.casatta.it:/var/www/eternitywall.com/html/
