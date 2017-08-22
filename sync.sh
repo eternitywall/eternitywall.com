31564ace2277ab2a49664be34d674f9bbb0d8ab0
Riccardo Casatta <riccardo@casatta.it>
2017-08-22 17:38:03 +0200

    added sync of authcal docs

--- sync.sh
+++ sync.sh
@@ -1,3 +1,4 @@
-jekyll build
 #spectacle -t api/pn2a/ -l img/logos/eternity_logo_final.svg api/pn2a/spec.yaml
+#spectacle -t api/authcal/ -l img/logos/eternity_logo_final.svg api/authcal/spec.yaml
+jekyll build
 rsync -vr --exclude node_modules/ _site/ riccardo.casatta.it:/var/www/eternitywall.com/html/
