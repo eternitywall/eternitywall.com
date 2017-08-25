872bbdd060290789c6ddbd3036c501e82f779617
Riccardo Casatta <riccardo@casatta.it>
2017-08-25 11:50:39 +0200

    api doc update

--- sync.sh
+++ sync.sh
@@ -1,4 +1,4 @@
-#spectacle -t api/pn2a/ -l img/logos/eternity_logo_final.svg api/pn2a/spec.yaml
-#spectacle -t api/authcal/ -l img/logos/eternity_logo_final.svg api/authcal/spec.yaml
+spectacle -t api/pn2a/ -l img/logos/eternity_logo_final.svg api/pn2a/spec.yaml
+spectacle -t api/authcal/ -l img/logos/eternity_logo_final.svg api/authcal/spec.yaml
 jekyll build
 rsync -vr --exclude node_modules/ _site/ riccardo.casatta.it:/var/www/eternitywall.com/html/
