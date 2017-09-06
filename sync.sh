f5a02dad305183c003af7c55db7eab188cbfee0c
Riccardo Casatta <riccardo@casatta.it>
2017-09-06 13:02:23 +0200

    adding privacy, vtoken page and sync to other server

--- sync.sh
+++ sync.sh
@@ -1,4 +1,5 @@
 spectacle -t api/pn2a/ -l img/logos/eternity_logo_final.svg api/pn2a/spec.yaml
 spectacle -t api/authcal/ -l img/logos/eternity_logo_final.svg api/authcal/spec.yaml
 jekyll build
-rsync -vr --exclude node_modules/ _site/ riccardo.casatta.it:/var/www/eternitywall.com/html/
+#rsync -vr --exclude node_modules/ _site/ riccardo.casatta.it:/var/www/eternitywall.com/html/
+rsync  -e 'ssh -p 2202' --progress -vr --exclude node_modules/ _site/ eternitywall.com:/var/www/eternitywall.com/
