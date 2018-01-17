697ce123c9ff8c6a09495db7261511061dc13c8f
Riccardo Casatta <riccardo@casatta.it>
2018-01-17 10:26:00 +0100

    removing sync.sh

--- sync.sh
+++ sync.sh
@@ -2,4 +2,5 @@ spectacle -t api/pn2a/ -l img/logos/eternity_logo_final.svg api/pn2a/spec.yaml
 spectacle -t api/authcal/ -l img/logos/eternity_logo_final.svg api/authcal/spec.yaml
 jekyll build
 #rsync -vr --exclude node_modules/ _site/ riccardo.casatta.it:/var/www/eternitywall.com/html/
-rsync  -e 'ssh -p 2202' --progress -vr --exclude node_modules/ _site/ eternitywall.com:/var/www/eternitywall.com/
+#rsync  -e 'ssh -p 2202' --progress -vr --exclude node_modules/ _site/ eternitywall.com:/var/www/eternitywall.com/
+rsync  --progress -vr --exclude node_modules/ _site/ root@eternitywall.com:/var/www/eternitywall.com/
