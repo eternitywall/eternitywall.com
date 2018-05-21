9f3be12c3069ae1ea312a5fc1fda809d37a16024
Riccardo Casatta <riccardo@casatta.it>
2018-05-21 12:03:58 +0200

    update spectacle

--- sync.sh
+++ sync.sh
@@ -1,5 +1,5 @@
-spectacle -t _site/api/pn2a/ -l img/logos/eternity_logo_final.svg api/pn2a/spec.yaml
-spectacle -t _site/api/authcal/ -l img/logos/eternity_logo_final.svg api/authcal/spec.yaml
+./node_modules/.bin/spectacle -t _site/api/pn2a/ -l img/logos/eternity_logo_final.svg api/pn2a/spec.yaml
+./node_modules/.bin/spectacle -t _site/api/authcal/ -l img/logos/eternity_logo_final.svg api/authcal/spec.yaml
 jekyll build
 #rsync -vr --exclude node_modules/ _site/ riccardo.casatta.it:/var/www/eternitywall.com/html/
 #rsync  -e 'ssh -p 2202' --progress -vr --exclude node_modules/ _site/ eternitywall.com:/var/www/eternitywall.com/
